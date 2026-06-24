a6f14fba2ab053fa951973497675d5609ccaddec
Supraja <suprajadb@gmail.com>
2026-06-24 10:58:05 +0530

    Factory Design Pattern

---
+++ FACTORY DESIGN PATTERN/simplefactory.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+using namespace std;
+
+class Burger{
+    public:
+    virtual void prepare()=0;
+    virtual ~Burger(){}
+
+};
+
+class BasicBurger:public Burger{
+    public:
+    void prepare() override {
+         cout << "Preparing Basic Burger with bun, patty, and ketchup!" << endl;
+    }
+
+};
+
+class StandardBurger: public Burger{
+    public:
+    void prepare()  override {
+          cout << "Preparing Standard Burger with bun, patty, cheese, and lettuce!" << endl;
+    }
+};
+
+class PremiumBurger : public Burger {
+public:
+    void prepare() override {
+        cout << "Preparing Premium Burger with gourmet bun, premium patty, cheese, lettuce, and secret sauce!" << endl;
+    }
+}; 
+
+class BurgerFactory{
+    public:
+    Burger* createBurger(string& type){
+       if(type=="basic"){
+        return new BasicBurger();
+       }
+      else if(type=="standard"){
+        return new StandardBurger();
+       }
+      else if(type=="premium"){
+        return new PremiumBurger();
+       }else{
+        cout<<"Invalid burger type!"<<endl;
+        return nullptr;
+       }
+    }
+
+};
+
+int main(){
+ string type="standard";
+ BurgerFactory*myBurgerFactory=new BurgerFactory();
+ Burger*burger=myBurgerFactory->createBurger(type);
+ burger->prepare();
+
+ return 0;
+};
